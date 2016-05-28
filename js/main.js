ee5ed87a77772340b013186fa060381705f48dd1
Onesuch <experts1994@mail.ru>
2016-05-28 11:13:04 +0300

    fixed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-var angle = 0;
-setInterval(function(){
-    angle+=5;
-    $('#secondPointer').rotate(angle);
-    $(angle).css({
-        'transform-origin' : 'right center 0',
-        'background' : '#000'
+var secondPointer = 90;
+var minutesPointer = 90;
+var hoverPointer = 90;
+
+setInterval(function () {
+    secondPointer += 0.4;
+    $("#secondPointer").rotate({
+        angle: secondPointer,
+        center: ["right", "center", "0"]
+    });
+}, 50);
+
+setInterval(function () {
+    minutesPointer += 0.0060000000;
+    $("#minutesPointer").rotate({
+        angle: minutesPointer,
+        center: ["right", "center", "0"]
+    });
+}, 50);
+
+setInterval(function () {
+    hoverPointer += 0.00003600000000;
+    $("#hoverPointer").rotate({
+        angle: hoverPointer,
+        center: ["right", "center", "0"]
     });
 }, 50);
